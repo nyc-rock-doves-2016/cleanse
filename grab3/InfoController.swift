b36c78208634082bbf80cce4f82c675fda0acb0e
Emily Osowski <emosowski@gmail.com>
2016-05-18 18:42:47 -0400

    Add info page

---
+++ grab3/InfoController.swift
@@ -0,0 +1,21 @@
+//
+//  InfoController.swift
+//  Cleanse
+//
+//  Created by Emily on 5/18/16.
+//  Copyright © 2016 emilyosowski. All rights reserved.
+//
+
+import UIKit
+
+class InfoController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.clearColor()
+        view.opaque = false
+    }
+    
+    @IBAction func buttonClicked(sender: AnyObject){
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}

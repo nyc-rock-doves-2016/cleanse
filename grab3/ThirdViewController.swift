2626c5a9c741682e9781daf70f9a01f2265afaf7
kevcorper <kevcorper@gmail.com>
2016-05-22 16:34:07 -0400

    refactor and clean up code--same functionality and design

--- grab3/ThirdViewController.swift
+++ grab3/ThirdViewController.swift
@@ -1,19 +1,3 @@
-//
-//  ThirdViewController.swift
-//  Cleanse
-//
-//  Created by Emily on 5/18/16.
-//  Copyright © 2016 emilyosowski. All rights reserved.
-//
-
-//
-//  SecondViewController.swift
-//  Cleanse
-//
-//  Created by Emily on 5/14/16.
-//  Copyright © 2016 emilyosowski. All rights reserved.
-//
-
 import UIKit
 import Photos
 
